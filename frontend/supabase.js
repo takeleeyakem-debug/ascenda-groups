8e4ea8362b3b73782c6dbda8effab9c7928c570f
takeleeyakem-debug <takeleeyakem@gmail.com>
2026-06-01 22:40:34 +0300

    Delete frontend directory

--- frontend/supabase.js
+++
@@ -1,5 +0,0 @@
-window.SUPABASE_URL = 'https://rfogomnbicceentrhymy.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJmb2dvbW5iaWNjZWVudHJoeW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyNDQwOTEsImV4cCI6MjA5NTgyMDA5MX0.wke5a7GEdU2oepfKfSD-rthLTEmpk3GqVfFRM_MjZ9c';
-window.LOGO_URL = 'https://rfogomnbicceentrhymy.supabase.co/storage/v1/object/public/Ascenda%20pics/logo.png';
-window.BANNER_URL = 'https://rfogomnbicceentrhymy.supabase.co/storage/v1/object/public/Ascenda%20pics/banner.png';
-window.supabaseClient = null;
